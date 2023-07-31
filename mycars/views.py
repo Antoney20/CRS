e68b2410a6d1cb9674fa184fedc006502a7c325c
ANTONY klerk <antomutwiri2001@gmail.com>
2023-07-31 22:10:08 +0300

    creating app, verify everythink is okay

--- mycars/views.py
+++ mycars/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+import requests
 # Create your views here.
+
+def index(requests):
+    return HttpResponse('Welcome index')
+    
+
