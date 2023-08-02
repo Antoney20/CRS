5e27e5f5fd50fe1d7ec2f98ca3a9a9ddf9420f17
ANTONY klerk <antomutwiri2001@gmail.com>
2023-08-02 15:46:37 +0300

    creating models

--- mycars/models.py
+++ mycars/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    USER_TYPE_CHOICES = (
+        ('user', 'User'),
+        ('driver', 'Driver'),
+    )
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.email
+    
+class Taxi(models.Model):
+    CAR_TYPE_CHOICES = (
+        ('sedan', 'Sedan'),
+        ('suv', 'SUV'),
+        ('van', 'Van'),
+        # Add more choices as needed
+    )
+
+    driver = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    car_type = models.CharField(max_length=10, choices=CAR_TYPE_CHOICES)
+    location = models.CharField(max_length=200)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.driver.full_name}'s {self.get_car_type_display()} - {self.location}"
--- mycars/views.py
+++ mycars/views.py
@@ -2,24 +2,47 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 import requests
 from django.conf import settings
+
+from .models import UserProfile
 # Create your views here.
 
-def index(requests):
-    return render(requests, 'mycars/index.html')
+def index(request):
+    return render(request, 'mycars/index.html')
 
 def contact(request):
-    return render(requests, 'mycars/contact.html')
+    return render(request, 'mycars/contact.html')
 
 def about(request):
-    return render(requests, 'mycars/about.html')
+    return render(request, 'mycars/about.html')
 
 def service(request):
-    return render(requests, 'mycars/service.html')
+    return render(request, 'mycars/service.html')
 
 def news(requests):
-    return render(requests, 'mycars/news.html')
-    
-    
+    return render(request, 'mycars/news.html')
+
+
+# myapp/views.py
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_type = request.POST.get('user_type')
+        full_name = request.POST.get('full_name')
+        phone_number = request.POST.get('phone_number')
+
+        UserProfile.objects.create(
+            email=email,
+            password=password,
+            user_type=user_type,
+            full_name=full_name,
+            phone_number=phone_number,
+        )
+        return redirect('index')
+
+    return render(request, 'myapp/signup.html')
+
     
     
     
